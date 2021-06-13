322b72d7bde6d0677fe6144c064d01ba087698ae
yskszk63 <yskszk63@gmail.com>
2021-06-13 19:31:50 +0900

    contract++

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,17 +19,17 @@ export default function Home({ data }) {
       {
         label: 'max',
         data: data.map(d => d.max),
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        backgroundColor: 'rgba(255, 99, 132, 1)',
       },
       {
         label: 'min',
         data: data.map(d => d.min),
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        backgroundColor: 'rgba(54, 162, 235, 1)',
       },
       {
         label: 'avg',
         data: data.map(d => d.avg),
-        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        backgroundColor: 'rgba(255, 206, 86, 1)',
       },
     ],
   };

3bfad68cce133f598321d15c1c66a3a1e010baf6
yskszk63 <yskszk63@gmail.com>
2021-06-13 19:39:37 +0900

    chage favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,8 +43,7 @@ export default function Home({ data }) {
       <Head>
         <title>yes-collection results</title>
         <meta name="description" content="Comparison of speeds of yes programs roughly written in each language." />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>⌛</text></svg>" />
       </Head>
 
       <AppBar>
